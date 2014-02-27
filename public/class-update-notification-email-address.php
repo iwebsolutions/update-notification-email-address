085c53af256d77fd836a9db5f77df266c879d5e8
Phil Wylie <philw@iweb.co.uk>
2014-02-27 23:58:05 +0000

    Update changelog and bump version.

--- public/class-update-notification-email-address.php
+++ public/class-update-notification-email-address.php
@@ -25,7 +25,7 @@ class Background_Update_Notification_Email_Address {
 	 *
 	 * @var string
 	 */
-	const VERSION = '1.0.1';
+	const VERSION = '1.0.2';
 
 	/**
 	 * Unique identifier for your plugin.
